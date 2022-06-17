cc0bdf6b1685f7d4118277431ba8a398b8a288e0
Alberto <aquintero88@gmail.com>
2022-06-17 22:35:00 +0200

    lab done

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
     type: String,
     unique: true
   },
-  hashedPasword: {
+  hashedPassword: {
     type: String,
   } 
 });
--- routes/auth.js
+++ routes/auth.js
@@ -27,7 +27,7 @@ router.post('/login', async (req, res, next) => {
             const passwordMatch = await bcrypt.compare(password, user.hashedPassword)
             if (passwordMatch) {
                 req.session.currentUser = user;
-                res.render ('index', user)
+                res.render ('auth/profile', user)
             } else {
                 res.render('auth/login', {error : 'wrong password or username'})
                 return;
@@ -47,18 +47,28 @@ router.post('/signup', async (req, res, next) => {
 
     const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
     if (!regex.test(password)) {
-        res.render('auth/signup', { error: 'Pasword must meet criteria'})
+        res.render('auth/signup', { error: 'Password must meet criteria'})
         return;
     }
     try {
         const salt = await bcrypt.genSalt(saltRounds);
         const hashedPassword = await bcrypt.hash(password, salt);
-        await User.create ({username, hashedPassword});
+        await User.create({username, hashedPassword});
         res.redirect('/auth/login')
         
     } catch (error) {
         next(error)        
     }
+})
+
+router.post('/logout', (req, res, next) => {
+    req.session.destroy((err) => {
+        if(err){
+            next(err)
+        } else {
+            res.redirect("/auth/login")
+        }
+    })
 })
 
 module.exports = router
